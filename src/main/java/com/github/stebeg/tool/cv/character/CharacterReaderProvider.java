c4c20cbce1e04b1f8e5b9ae83005ad48cd5ee2fa
stebeg <steffenberger85@web.de>
2020-03-19 13:49:39 +0100

    Added code comments

--- src/main/java/com/github/stebeg/tool/cv/character/CharacterReaderProvider.java
+++ src/main/java/com/github/stebeg/tool/cv/character/CharacterReaderProvider.java
@@ -3,6 +3,12 @@ package com.github.stebeg.tool.cv.character;
 import com.github.stebeg.tool.cv.common.UrlContentReaderProvider;
 import com.google.gson.Gson;
 
+/**
+ * Provides an instance of the implementation of the {@link CharacterReader} interface for reading
+ * information from the Comicvine API.
+ *
+ * @author Steffen Berger
+ */
 public class CharacterReaderProvider {
 
   private static final CharacterReader CHARACTER_READER = new CharacterReaderImpl(
@@ -14,6 +20,10 @@ public class CharacterReaderProvider {
     throw new UnsupportedOperationException();
   }
 
+  /**
+   * @return The only instance of the implementation of {@link CharacterReader}.
+   * @see CharacterReader
+   */
   public static final CharacterReader getInstance() {
     return CHARACTER_READER;
   }
