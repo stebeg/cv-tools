159263845f4a422bf6ab9da921197f9f41d6d9b8
Stebeg <steffenberger85@web.de>
2024-12-25 15:32:06 +0100

    Refactoring of retrieving publishers

--- src/main/java/com/github/stebeg/comicvine/ComicvineTools.java
+++ src/main/java/com/github/stebeg/comicvine/ComicvineTools.java
@@ -3,11 +3,19 @@ package com.github.stebeg.comicvine;
 import com.github.stebeg.comicvine.character.CharacterRetriever;
 import com.github.stebeg.comicvine.common.request.UrlConnectionBuilder;
 import com.github.stebeg.comicvine.common.request.UrlContentReader;
+import com.github.stebeg.comicvine.publisher.PublisherRetriever;
 import com.google.gson.Gson;
 
+/**
+ * Provides access to the Comicvine API.
+ */
 public class ComicvineTools {
 
   private static final CharacterRetriever CHARACTER_RETRIEVER = new CharacterRetriever(
+      new UrlContentReader(new UrlConnectionBuilder()),
+      new Gson());
+
+  private static final PublisherRetriever PUBLISHER_RETRIEVER = new PublisherRetriever(
       new UrlContentReader(new UrlConnectionBuilder()),
       new Gson());
 
@@ -18,6 +26,15 @@ public class ComicvineTools {
    */
   public static CharacterRetriever getCharacterRetriever() {
     return CHARACTER_RETRIEVER;
+  }
+
+  /**
+   * Retrieves a singleton instance of the {@link PublisherRetriever}.
+   *
+   * @return A singleton instance of the {@link PublisherRetriever}.
+   */
+  public static PublisherRetriever getPublisherRetriever() {
+    return PUBLISHER_RETRIEVER;
   }
 
 }
